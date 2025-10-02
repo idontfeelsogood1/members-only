1b9c6b7b41a89180bb23a0b89af3ff61deb12856
Le Duong <leduongnewmemes123@gmail.com>
2025-10-02 17:03:14 +0700

    feat: Configure passport

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const db = requie('../db/queries')
+const bcrypt = require('bcrypt')
+
+const verifyCallBack = async (username, password, done) => {
+    try {
+        const user = await db.getUserByUsername(username) 
+        const hash = await db.getHash(user.id)
+        if (user.length <= 0) {
+            return done(null, false)
+        } else if (bcrypt.compare(password, hash)) {
+            return done(null, user[0])
+        } else {
+            return done(null, false)
+        }
+    } catch(err) {
+        console.log("Error at verifyCallback: ")
+        throw new Error(err)
+    }
+}
+
+passport.use(new LocalStrategy(verifyCallBack))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async (userId, done) => {
+    try {
+        const user = await db.getUser(userId)
+        done(null, user)
+    } catch(err) {
+        console.log("Error at deserializeUser: ")
+        throw new Error(err)
+    }
+})
