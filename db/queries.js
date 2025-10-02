761b953dca817e72cdcf4c27e7f1460d41596bcb
Le Duong <leduongnewmemes123@gmail.com>
2025-10-02 17:53:53 +0700

    feat: Implement getUserByUsername, getUser, getHash

--- db/queries.js
+++ db/queries.js
@@ -20,6 +20,48 @@ async function getUsersAndMessages() {
     }
 }
 
+async function getUserByUsername(username) {
+    try {
+        const { rows } = await db.query(`
+            SELECT * FROM users
+            WHERE username = $1    
+        `, [username])
+        return rows[0]
+    } catch(err) {
+        console.log("DB error in getUserByUsername: ", err)
+        throw new Error(err)
+    }
+}
+
+async function getUser(user_id) {
+    try {
+        const { rows } = await db.query(`
+            SELECT * FROM users
+            WHERE id = $1    
+        `, [user_id])
+        return rows[0]
+    } catch(err) {
+        console.log("DB error in getUser: ", err)
+        throw new Error(err)
+    }
+}
+
+async function getHash(user_id) {
+    try {
+        const { rows } = await db.query(`
+            SELECT password_hash FROM users
+            WHERE id = $1    
+        `, [user_id])
+        return rows[0].password_hash
+    } catch(err) {
+        console.log("DB error in getHash: ", err)
+        throw new Error(err)
+    }
+}
+
 module.exports = {
     getUsersAndMessages,
+    getUserByUsername,
+    getUser,
+    getHash,
 }
