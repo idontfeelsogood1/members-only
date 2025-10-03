2ccd91134d7b249d2b868b16480c721abba1fcc2
Le Duong <leduongnewmemes123@gmail.com>
2025-10-03 14:36:43 +0700

    feat: Implement validateRegisterMiddlewares array, validateRegister

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require('../db/queries')
+const { body, validationResult } = require('express-validator')
 
 async function indexGet(req, res, next) {
     try {
@@ -22,8 +23,60 @@ async function registerGet(req, res, next) {
     res.render('register')
 }
 
+const validateRegisterMiddlewares = [
+    body('firstname')
+        .trim()
+        .notEmpty()
+        .withMessage('First name cannot be empty.')
+        .matches(/^[A-Za-z\s]+$/) 
+        .withMessage('First name must contain only letters and spaces.')
+        .isLength({ min: 2, max: 12 })
+        .withMessage('First name must be between 2 and 12 characters long.'),
+
+    body('lastname')
+        .trim()
+        .notEmpty()
+        .withMessage('Last name cannot be empty.')
+        .matches(/^[A-Za-z\s]+$/) 
+        .withMessage('Last name must contain only letters and spaces.')
+        .isLength({ min: 2, max: 12 })
+        .withMessage('Last name must be between 2 and 12 characters long.'),
+
+    body('username')
+        .trim()
+        .notEmpty()
+        .withMessage('Username cannot be empty.')
+        .isLength({ min: 8, max: 20 })
+        .withMessage('Username must be between 8 and 20 characters long.'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password cannot be empty.')
+        .matches(/^\S*$/)
+        .withMessage('Password cannot contain spaces')
+        .isLength({ min: 8, max: 20 })
+        .withMessage('Password must be between 8 and 20 characters long.'),
+
+    body('confirm-password')
+        .custom((value, { req }) => {
+            return value === req.body.password;
+        })
+        .withMessage('Password must match Confirm Password.')
+]
+
+
+function validateRegister(req, res, next) {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+        res.status(400).render('register', { errors: result.array() })
+    } else {
+        next()
+    }
+}
 
 module.exports = {
     indexGet,
     registerGet,
+    validateRegisterMiddlewares,
+    validateRegister,
 }
